a353ff7657472fcd226a97c9c53fdfc496a386dc
s-kvng <kvngnathan8420@gmail.com>
2023-11-27 02:25:55 +0000

    create chat feed component

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+const ChatFeed = (props) => {
+  console.log(props);
+  return <div>ChatFeed</div>;
+};
+
+export default ChatFeed;
