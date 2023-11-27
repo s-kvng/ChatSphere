cf0607edc44c295d5610d1b51e9cb139fbe09b2a
s-kvng <kvngnathan8420@gmail.com>
2023-11-27 03:36:07 +0000

    add my message component and their message component to chatfeed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -5,8 +5,63 @@ import MyMessage from "./MyMessage";
 import TheirMessage from "./TheirMessage";
 
 const ChatFeed = (props) => {
-  console.log(props);
-  return <div>ChatFeed</div>;
+  const { chats, activeChat, userName, messages } = props;
+  console.log(chats, userName, messages);
+
+  const chat = chats && chats[activeChat];
+  console.log(messages);
+  const renderMessages = () => {
+    const keys = Object.keys(messages);
+    console.log(keys);
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isMyMessage ? (
+              <MyMessage message={message} />
+            ) : (
+              <TheirMessage message={message} lastMessage={lastMessageKey} />
+            )}
+          </div>
+          <div
+            className="read-receipts"
+            style={{
+              marginRight: isMyMessage ? "18px" : "0px",
+              marginLeft: isMyMessage ? "0px" : "68px",
+            }}
+          >
+            {" "}
+            read receipts
+          </div>
+        </div>
+      );
+    });
+  };
+
+  renderMessages();
+
+  if (!chat) return "Loading...";
+
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => `${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: "100px" }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  );
 };
 
 export default ChatFeed;
